3b504d083921d932f4a4ceca423d40fde4c72b08
Mathias-Mourgues-ISEN <mathias.mourgues@isen.yncrea.fr>
2024-03-12 14:45:20 +0100

    update build

--- app/src/main/java/com/snowaze/app/MainActivity.kt
+++ app/src/main/java/com/snowaze/app/MainActivity.kt
@@ -6,13 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.snowaze.app.compose.navigation.DrawerContent
-import com.snowaze.app.compose.navigation.items
-import com.snowaze.app.screens.auth.signup.SignUpScreen
 import com.snowaze.app.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
