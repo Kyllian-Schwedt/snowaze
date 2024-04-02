e68b6d03d7df22c34ad0cdb2cca45aeee31f8949
Kyllian SCHWEDT <kyllian@schwedt.fr>
2024-04-03 01:22:56 +0200

    +pager

---
+++ app/src/main/java/com/snowaze/app/common/utils/pager/PagerScope.kt
@@ -0,0 +1,24 @@
+package com.snowaze.app.common.utils.pager
+
+class PagerScope(
+    private val state: PagerState,
+    val commingPage: Int
+) {
+    /**
+     * Returns the current selected page
+     */
+    val currentPage: Int
+        get() = state.currentPage
+
+    /**
+     * Returns the current selected page offset
+     */
+    val currentPageOffset: Float
+        get() = state.currentPageOffset
+
+    /**
+     * Returns the current selection state
+     */
+    val selectionState: SelectionState
+        get() = state.selectionState
+}
