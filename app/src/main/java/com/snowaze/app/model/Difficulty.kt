0d2a6df303d7e10c5918128193909714651f144a
Kyllian SCHWEDT <kyllian@schwedt.fr>
2024-04-07 20:11:49 +0200

    + String value

--- app/src/main/java/com/snowaze/app/model/Difficulty.kt
+++ app/src/main/java/com/snowaze/app/model/Difficulty.kt
@@ -1,8 +1,17 @@
 package com.snowaze.app.model
 
 enum class Difficulty {
+
     GREEN,
     BLUE,
     RED,
-    BLACK
+    BLACK;
+
+    val value: String
+        get() = when (this) {
+            GREEN -> "Green"
+            BLUE -> "Blue"
+            RED -> "Red"
+            BLACK -> "Black"
+        }
 }
